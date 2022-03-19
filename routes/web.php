b136c03a75e2898b77192e2ab684cdd7164388b4
Ahmed Alghoul <Ahmed.Alghoul98@gmail.com>
2022-03-19 15:56:24 +0200

    Added Department cmfs

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\DoctorsController;
 use App\Http\Controllers\NurseController;
 use App\Http\Controllers\HospitalController;
+use Database\Factories\DepartmentFactory;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,12 +30,11 @@ Route::prefix('cms/admin')->group(function () {
     Route::view('login', 'cms.login');
     Route::view('register', 'cms.register');
     Route::view('parent', 'cms.parent')->name('cms.parent');
-    Route::view('form', 'cms.doctors.form');
-    Route::view('doctor', 'cms.doctors.index');
     Route::view('/', 'cms.dashboard')->name('cms.dashboard');
     Route::resource('doctor', DoctorsController::class);
     Route::resource('nurse', NurseController::class);
     Route::resource('hospital', HospitalController::class);
+    Route::resource('department', DepartmentController::class);
 });
 
 Route::get('/', function () {
