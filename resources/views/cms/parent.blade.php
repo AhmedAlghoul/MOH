ebf9b00df12d686ba9807d7609707be5bf97975f
Ahmed Alghoul <Ahmed.Alghoul98@gmail.com>
2022-03-19 16:03:26 +0200

    changed route of the logo to dashboard

--- resources/views/cms/parent.blade.php
+++ resources/views/cms/parent.blade.php
@@ -111,7 +111,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
+      <a href="{{route('cms.dashboard')}}" class="brand-link">
         <img src="{{asset('cms/dist/img/moh.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
           style="opacity: .8">
         <span class="brand-text font-weight-light">وزارة الصحة</span>
