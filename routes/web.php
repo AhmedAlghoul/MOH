29b718f7bd3febd4b4082f9f9f5fabfd5d98f56c
Ahmed Alghoul <Ahmed.Alghoul98@gmail.com>
2022-06-06 08:34:40 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\DoctorsController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\NurseController;
 use App\Http\Controllers\HospitalController;
+use App\Http\Controllers\KeyController;
 use App\Http\Controllers\UserAuthController;
+use App\Http\Controllers\RoleController;
+use App\Models\KeyCalculate;
 use Database\Factories\DepartmentFactory;
 use Illuminate\Support\Facades\Route;
 
@@ -43,9 +47,28 @@ Route::prefix('cms/admin')->middleware('auth:admin')->group(function () {
     Route::view('register', 'cms.register')->name('cms.register');
     Route::view('parent', 'cms.parent')->name('cms.parent');
     Route::view('/', 'cms.dashboard')->name('cms.dashboard');
-    Route::resource('doctor', DoctorsController::class);
     Route::resource('nurse', NurseController::class);
     Route::resource('hospital', HospitalController::class);
     Route::resource('department', DepartmentController::class);
+    Route::resource('role', RoleController::class);
+    Route::resource('key', KeyController::class);
+    Route::resource('employee', EmployeeController::class);
+    Route::resource('keycalc', KeyCalculate::class);
+    Route::view('keycalc', 'cms.keycalc.form')->name('cms.keycalc');
+    Route::view('nursecalc', 'cms.keycalc.nursecalc')->name('nursecalc');
+    Route::view('physicaltherapycalc', 'cms.keycalc.physicaltherapycalc')->name('physicaltherapycalc');
+    Route::view('medicalimagingcalc', 'cms.keycalc.medicalimagingcalc')->name('medicalimagingcalc');
+    Route::view('pharmacycalc', 'cms.keycalc.pharmacycalc')->name('pharmacycalc');
+    Route::view('laboratorycalc', 'cms.keycalc.laboratorycalc')->name('laboratorycalc');
+    Route::view('doctorcalc', 'cms.keycalc.doctorcalc')->name('doctorcalc');
+    Route::view('administrativecalc', 'cms.keycalc.administrativecalc')->name('administrativecalc');
+    Route::view('calc', 'cms.nurses.calculate')->name('cms.nurses.calculate');
     Route::get('logout', [UserAuthController::class, 'logout'])->name('cms.logout');
+    //nurses key index and create key views
+    Route::view('nurseskey', 'cms.nurses.key')->name('cms.nurseskey');
+    Route::view('/addnurseskey', 'cms.nurses.addkey')->name('cms.addnurseskey');
+    //laravel excel
+    Route::get('file-import-export', [EmployeeController::class, 'fileImportExport']);
+    Route::post('file-import', [EmployeeController::class, 'fileImport'])->name('file-import');
+    Route::get('file-export', [EmployeeController::class, 'fileExport'])->name('file-export');
 });
