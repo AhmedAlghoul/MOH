6164f14d0095644b1a4dded18076044255287c3d
Ahmed Alghoul <Ahmed.Alghoul98@gmail.com>
2022-07-31 11:22:42 +0300

    add soft delete to department model

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -4,15 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Department extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $fillable = [
         'name',
         'is_active'
     ];
     protected $table = 'departments';
+    
     //write relation to has many hospitals
     // public function hospitals()
     // {
