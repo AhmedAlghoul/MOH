3b8b0c92bc3359f5a6e85ef1be394e9239474ff9
Ahmed Alghoul <Ahmed.Alghoul98@gmail.com>
2022-10-05 12:14:55 +0300

    added circle relation

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\CircleController;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,7 +10,7 @@ class Employee extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'job_number', 'employee_name', 'hospital_id', 'department_id', 'role_id', 'mobile_number'
+        'job_number', 'employee_name', 'hospital_id', 'department_id', 'circle_id', 'role_id', 'mobile_number',
     ];
     public function departments()
     {
@@ -22,5 +23,9 @@ class Employee extends Model
     public function EmployeesRoles()
     {
         return $this->belongsTo(EmployeeRole::class, 'role_id', 'id');
+    }
+    public function Circles()
+    {
+        return $this->belongsTo(circle::class, 'circle_id', 'id');
     }
 }
