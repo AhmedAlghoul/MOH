abffd2919d26893739e799f7bb2e4f7e775899de
Ahmed Alghoul <Ahmed.Alghoul98@gmail.com>
2022-10-05 11:45:38 +0300

    spatie controllers

---
+++ app/Http/Controllers/RolePermissionController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Dotenv\Validator;
+use Illuminate\Http\Request;
+use Spatie\Permission\Contracts\Permission;
+use Spatie\Permission\Models\Permission as ModelsPermission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($roleId)
+    {
+        //
+        $permissions = ModelsPermission::paginate(20);
+        return response()->view('cms.spatie.roles.role-permissions', ['role_id' => $roleId, 'permissions' => $permissions]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $roleId)
+    {
+        //
+        $validator = Validator($request->all(), [
+            'permission_id' => 'required|exists:permissions,id|numeric'
+        ]);
+
+        if (!$validator->fails()) {
+            $role = Role::findById($roleId);
+        } else {
+            return response()->json(['message' => $validator->getMessageBag()->first()], 400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
