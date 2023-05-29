183076c615eea57d4b7f890ee4a924471a1695c3
Ahmed Alghoul <Ahmed.Alghoul98@gmail.com>
2023-05-29 23:15:25 +0300

    composer update

--- vendor/yajra/laravel-oci8/src/Oci8/Schema/OracleAutoIncrementHelper.php
+++ vendor/yajra/laravel-oci8/src/Oci8/Schema/OracleAutoIncrementHelper.php
@@ -83,7 +83,7 @@ class OracleAutoIncrementHelper
     }
 
     /**
-     * Create an object name that limits to 30 chars.
+     * Create an object name that limits to 30 or 128 chars depending on the server version.
      *
      * @param  string  $prefix
      * @param  string  $table
@@ -93,8 +93,9 @@ class OracleAutoIncrementHelper
      */
     private function createObjectName($prefix, $table, $col, $type)
     {
-        // max object name length is 30 chars
-        return substr($prefix.$table.'_'.$col.'_'.$type, 0, 30);
+        $maxLength = $this->connection->getSchemaGrammar()->getMaxLength();
+
+        return substr($prefix.$table.'_'.$col.'_'.$type, 0, $maxLength);
     }
 
     /**
--- vendor/yajra/laravel-pdo-via-oci8/CHANGELOG.md
+++ vendor/yajra/laravel-pdo-via-oci8/CHANGELOG.md
@@ -2,6 +2,15 @@
 
 ## [UNRELEASED]
 
+## v3.3.1 - 2023-03-15
+
+- fix: getAttribute returning array instead of null #124
+- fix #123
+
+## v3.3.0 - 2023-02-20
+
+- feat: stringify support in pdo via oci 8 #122
+
 ## v3.2.5 - 2022-12-21
 
 - fix: Don't suppress exceptions when connecting #121
